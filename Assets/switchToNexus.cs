3ce43443751ed6c2b5935654d4fc490253b057b0
StrawberryNougat <eleanormather02@gmail.com>
2020-11-12 22:23:27 -0500

    Oh boy I was working in the wrong branch
    Had to discard some changes but I guess that's what caffeine is for

---
+++ Assets/switchToNexus.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//using UnityEngine.SceneManager;
+
+public class switchToNexus : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void onEnable()
+    {
+        Application.LoadLevel("PlaceHolderScene");
+    }
+
+    // Update is called once per frame
+   
+}
